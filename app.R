6438508b7d8eddfc9b48e493c3f8c8223f49b8be
ncuran <146478252+ncuran@users.noreply.github.com>
2023-12-10 21:20:33 -0800

    Update app.R

--- app.R
+++ app.R
@@ -8,39 +8,47 @@
 #
 
 library(shiny)
-df<-read.csv("df combined.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Who Had The Best Hitting Season in MLB History"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      plotOutput("plot1",click = "plot_click"),
-      verbatimTextOutput("info")
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
+
+    # Application title
+    titlePanel("Frequency of High Caliber Seasons"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+          radioButtons(
+            inputId = "hit",
+            label = "how many times did these famous hitters appear in the dataset?",
+            choices = list("Barry Bonds" = 1, "Babe Ruth" = 2, "Ty Cobb"=3, "Alber Pujols"=4, "Dan Musial"=5, "Rodgers Hornsby"=6),
+          )
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           textOutput(outputId="hitScore")
+        )
     )
-  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  output$Plot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    plot(df$player_szn, df$OPS)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white',
-         xlab = 'Waiting time to next eruption (in mins)',
-         main = 'Histogram of waiting times')
+
+  output$hitScore <- renderText({
+    if(input$hit == 1){
+      return("7 times")
+    } else if(input$hit ==2){
+      return("10 times")
+    }else if(input$hit ==3){
+      return("6 times")
+    }else if(input$hit==4){
+      return("3 times")
+    }else if(input$hit==5){
+      return("3 times")
+    }else{
+      return("7 times")
+    }
   })
 }
 
